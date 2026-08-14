0cb26909123b12ef2ec42003e9fe5ec60ee4cc74
RyotaMiura1105 <a22.3ytd@g.chuo-u.ac.jp>
2026-08-15 01:30:49 +0900

    commit

--- server/server.js
+++ server/server.js
@@ -5,6 +5,20 @@ require("dotenv").config();
 
 const app = express();
 
+const cors = require("cors");
+
+// VercelのURL（またはすべてのドメインを許可する場合は '*'）を設定
+const corsOptions = {
+  origin: [
+    "https://inventory-lilac-sigma.vercel.app/",
+    "http://localhost:5173",
+  ], // ここにVercelのURLを入れる
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 // ミドルウェアの設定
 app.use(express.json()); // JSON形式のリクエストボディを解析できるようにする
 app.use(cors()); // Reactからのクロスドメイン通信を許可する
