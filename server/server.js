40eb63ad1b8c9e292ed62807357aa2be258a8671
RyotaMiura1105 <a22.3ytd@g.chuo-u.ac.jp>
2026-08-15 02:01:07 +0900

    commit

--- server/server.js
+++ server/server.js
@@ -116,7 +116,9 @@ app.put("/api/scans/checked", (req, res) => {
 });
 
 // サーバーの起動
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Backend is running on port ${PORT}`);
+initDb().then(() => {
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(`Backend is running on port ${PORT}`);
+  });
 });
